8c39800a3bfc73df33ad52679f3f4890b15cac30
Venugopalan <iyengarvenugopalan@gmail.com>
2020-10-28 11:18:34 +0530

    First thirteen exercises done

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called",script #This gets the first element in the argv array of args
+print "The first variable is:",first
+print 'The second variable is:',second
+print "The third variable is:",third
+print argv
